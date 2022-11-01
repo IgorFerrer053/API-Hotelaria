fa569e7eab262ca5259852a50a1626de81225248
IgorFerrer053 <Igorsantfer@gmail.com>
2022-11-01 01:10:28 -0300

    fist commit

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
 # API-Hotelaria
+API REST com JSON Server 
+
+## Clone o repositório do GitHub no Git
+$ git clone <https://github.com/IgorFerrer053/API-Hotelaria>
+
+## Instale as dependências
+$ npm install
+
+## Execute o script
+$ npm run start
+
+## Acesse o servidor local
+$ <http://localhost:3000>
---
+++ server.js
@@ -0,0 +1,10 @@
+import jsonServer from 'json-server'
+const server = jsonServer.create()
+const router = jsonServer.router('db.json')
+const middlewares = jsonServer.defaults()
+
+server.use(middlewares)
+server.use(router)
+server.listen(3000, () => {
+  console.log('Servidor ativo:http://localhost:3000')
+})
